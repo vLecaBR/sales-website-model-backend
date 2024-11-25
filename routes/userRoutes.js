103924b37363e9af1f82f441030479c0563bd42e
vlecabr <vitartasleca@gmail.com>
2024-11-25 12:14:57 -0300

    comentarios adicionados

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-// Rotas de usuários
-router.post('/cadastro', userController.register);
-router.post('/login', userController.login);
-router.put('/update', authMiddleware, userController.update);
+//! Rotas de usuários
+router.post('/cadastro', userController.register); //! Rota pública
+router.post('/login', userController.login); //! Rota pública
+router.put('/update', authMiddleware, userController.update); //! Rota protegida
 
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -1,35 +1,35 @@
 const Product = require('../model/product');
 
-const getAllProducts = async () => {
-  return await Product.findAll();
+const getAllProducts = async () => { //! Função para pegar todos os produtos
+  return await Product.findAll();  //! Busca todos os produtos
 };
 
-const getProductById = async (id) => {
-  return await Product.findByPk(id);
+const getProductById = async (id) => { //! Função para pegar um produto pelo ID
+  return await Product.findByPk(id); //! Busca o produto pelo ID
 };
 
-const createProduct = async (productData) => {
-  return await Product.create(productData);
+const createProduct = async (productData) => { //! Função para criar um novo produto
+  return await Product.create(productData); //! Cria um novo produto
 };
 
-const updateProduct = async (id, productData) => {
-  const product = await Product.findByPk(id);
-  if (product) {
-    return await product.update(productData);
+const updateProduct = async (id, productData) => { //! Função para atualizar um produto existente
+  const product = await Product.findByPk(id); //! Busca o produto pelo ID
+  if (product) { //! Caso o produto não seja encontrado
+    return await product.update(productData); //! Atualiza o produto
   }
   return null;
 };
 
-const deleteProduct = async (id) => {
-  const product = await Product.findByPk(id);
-  if (product) {
-    await product.destroy();
-    return true;
+const deleteProduct = async (id) => { //! Função para deletar um produto
+  const product = await Product.findByPk(id); //! Busca o produto pelo ID
+  if (product) { //! Caso o produto não seja encontrado
+    await product.destroy(); //! Deleta o produto
+    return true; 
   }
   return false;
 };
 
-module.exports = {
+module.exports = { //! Exporta as funções
   getAllProducts,
   getProductById,
   createProduct,
