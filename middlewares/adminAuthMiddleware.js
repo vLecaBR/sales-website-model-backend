26595e84afd6cf1e7e424aaf6d276b00039cc383
vlecabr <vitartasleca@gmail.com>
2024-11-25 12:08:51 -0300

    comentarios adicionados

--- middlewares/adminAuthMiddleware.js
+++ middlewares/adminAuthMiddleware.js
@@ -1,28 +1,28 @@
 // middlewares/adminAuthMiddleware.js
 const jwt = require('jsonwebtoken');
 
-// Middleware para autenticação de administradores no AdminJS
+//! Middleware para autenticação de administradores no AdminJS
 const adminAuthMiddleware = async (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Extrai o token do cabeçalho
+  const authHeader = req.headers['authorization']; //! Pega o cabeçalho de autorização
+  const token = authHeader && authHeader.split(' ')[1]; //! Extrai o token do cabeçalho
 
-  if (!token) {
+  if (!token) { //! Caso não tenha token retorna mensagem de erro
     return res.status(401).json({ message: 'Token não fornecido.' });
   }
 
-  try {
-    const user = jwt.verify(token, process.env.JWT_SECRET || 'seu_segredo');
+  try { //! Tenta verificar o token do usuário
+    const user = jwt.verify(token, process.env.JWT_SECRET || 'seu_segredo'); //! Verifica o token com a chave secreta
     
-    // Verifica se o usuário é administrador
-    if (user.role !== 'admin') { // Aqui é onde a verificação ocorre
+    //! Verifica se o usuário é administrador
+    if (user.role !== 'admin') { //! Aqui é onde a verificação ocorre
       return res.status(403).json({ message: 'Acesso negado. Somente administradores podem acessar.' });
     }
 
-    req.user = user;  // Adiciona o usuário ao objeto req para o próximo middleware
-    next();
+    req.user = user;  //! Adiciona o usuário ao objeto req para o próximo middleware
+    next(); 
   } catch (err) {
     return res.status(403).json({ message: 'Token inválido.' });
   }
 };
 
-module.exports = adminAuthMiddleware; // Exporta o middleware
+module.exports = adminAuthMiddleware; //! Exporta o middleware
