d012e5f09ef471d62c9b008f13973f1325679402
vlecabr <vitartasleca@gmail.com>
2024-11-25 11:56:27 -0300

    comentarios adicionados

--- config/database.js
+++ config/database.js
@@ -2,17 +2,17 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', // Caminho para o arquivo do banco SQLite
+  dialect: 'sqlite', //! Tipo de banco de dados
+  storage: './database.sqlite', //! Caminho do arquivo do banco de dados
 });
 
-const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Conexão com o banco de dados foi estabelecida com sucesso.');
-  } catch (error) {
+const connectDB = async () => { //! Função para conectar ao banco de dados
+  try { //! Tenta conectar ao banco de dados
+    await sequelize.authenticate(); //! Função para autenticar a conexão com o banco de dados
+    console.log('Conexão com o banco de dados foi estabelecida com sucesso.'); 
+  } catch (error) { //! Caso ocorra um erro ao conectar ao banco de dados
     console.error('Erro ao conectar com o banco de dados:', error);
   }
 };
 
-module.exports = { sequelize, connectDB }; // Exporta o sequelize e connectDB
+module.exports = { sequelize, connectDB }; //! Exporta o sequelize e connectDB
