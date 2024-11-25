1b26fd84005579c100dc5c139b58cf635f667303
vlecabr <vitartasleca@gmail.com>
2024-11-25 12:09:29 -0300

    comentarios adicionados

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,18 +2,18 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Extrai o token após "Bearer "
+  const authHeader = req.headers['authorization']; //! Pega o cabeçalho de autorização
+  const token = authHeader && authHeader.split(' ')[1]; //! Extrai o token após "Bearer "
 
-  if (!token) {
-    return res.status(401).json({ message: 'Token não fornecido.' });
+  if (!token) { //! Caso não tenha token retorna mensagem de erro
+    return res.status(401).json({ message: 'Token não fornecido.' }); 
   }
 
-  jwt.verify(token, process.env.JWT_SECRET || 'seu_segredo', (err, user) => {
-    if (err) {
+  jwt.verify(token, process.env.JWT_SECRET || 'seu_segredo', (err, user) => { //! Verifica o token com a chave secreta
+    if (err) { //! Caso ocorra um erro retorna mensagem de erro
       return res.status(403).json({ message: 'Token inválido.' });
     }
-    req.user = user;
+    req.user = user; //! Adiciona o usuário ao objeto req para o próximo middleware
     next();
   });
 };
